436fc87817b41f4fd84cbad463c09a447af03b2c
preetimath <mathpreeti24@gmail.com>
2021-10-09 21:32:13 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      A cocktail is a term relating to any type of alcoholic mixed drink. Most commonly, cocktails are either a combination of spirits, or one or more spirits mixed with other ingredients such as fruit juice, flavored syrup, or cream. Cocktails vary widely across regions of the world, and many websites publish both original recipes and their own interpretations of older and more famous cocktails.
+      </p>
+      <p>
+      This Web App is collection of cocktails using cocktailsDB API and some cocktails can be searched. The details of particular cocktail can be viewed. The details include category, glass, instructions to follow, ingredients used.
+      </p>
+    </section>
+  )
+}
